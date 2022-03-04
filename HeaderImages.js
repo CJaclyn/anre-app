0bc9a9c16d089e9b682abe6ed1fd277ee5dfed20
Moon Fish <daivietnam@protonmail.com>
2022-03-04 11:39:53 -0600

    change index header images

--- HeaderImages.js
+++ HeaderImages.js
@@ -1,12 +1,9 @@
 export const HeaderImages = [
     '/images/header/1.jpg', 
     '/images/header/2.jpg',  
-    '/images/header/3.jpeg',
+    '/images/header/3.jpg',
     '/images/header/4.jpg',
     '/images/header/5.jpg',
-    '/images/header/6.jpeg',
+    '/images/header/6.jpg',
     '/images/header/7.jpg',
-    '/images/header/8.jpg',
-    '/images/header/9.jpg',
-    '/images/header/10.jpg',
 ]
